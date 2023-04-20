da8dc3499a414c8f3d781595ee9d998feaf061ef
Vijay Kumar <vijay_kumar@isb.edu>
2023-04-20 11:29:48 +0530

    add fuzzy_cosine_scores

--- bipp/lgd/algorithms.py
+++ bipp/lgd/algorithms.py
@@ -7,7 +7,7 @@ from typing import List
 # %%
 
 
-def fuzzy_cosine_match(input_name: str, candidate_names: List[str], similarity=0.75, fuzzy_match=0.7, errors="ignore"):
+def fuzzy_cosine_match(input_name: str, candidate_names: List[str], errors="raise",**kwargs):
     """
     Compares an input name to a list of candidate names
     using both fuzzy search and cosine similarity,
@@ -25,20 +25,8 @@ def fuzzy_cosine_match(input_name: str, candidate_names: List[str], similarity=0
     """
 
     try:
-        # Compute the fuzzy search score
-        fuzzy_scores = [fuzz.token_sort_ratio(
-            name, input_name) for name in candidate_names]
-
-        # Compute the cosine similarity score
-        vectorizer = CountVectorizer().fit_transform(
-            [input_name] + candidate_names)
-        cosine_similarities = cosine_similarity(
-            vectorizer[0:1], vectorizer[1:]).flatten()
-
-        # Combine the fuzzy search and cosine similarity scores
-        scores = [fuzzy_match * fuzzy_scores[i] + similarity *
-                  cosine_similarities[i] for i in range(len(candidate_names))]
-
+        scores = fuzzy_cosine_scores(input_name=input_name, candidate_names=candidate_names, **kwargs)
+        scores = [score[1] for score in scores]
         # Find the best matching name based on the combined score
         best_match = candidate_names[scores.index(max(scores))]
 
@@ -50,5 +38,42 @@ def fuzzy_cosine_match(input_name: str, candidate_names: List[str], similarity=0
             raise e
         else:
             return None
+
+# %%
+
+def fuzzy_cosine_scores(input_name: str, candidate_names: List[str], similarity=0.75, fuzzy_match=0.7):
+    """
+    Compares an input name to a list of candidate names
+    using both fuzzy search and cosine similarity.
+
+    Args:
+        input_name (str): The input name for which the best match needs to be found.
+        candidate_names (list[str]): A list of candidate names.
+        similarity (float, optional): The weightage of cosine similarity score (default 0.75).
+        fuzzy_match (float, optional): The weightage of fuzzy matcher (default 0.7).
+
+    Returns:
+        (list[tuple[str, float]]): list of pairs of candidate names and their similarity scores.
+    """
+    is_str = lambda v: type(v) == str
+
+    if is_str(input_name) and all(map(is_str, candidate_names)):
+        raise ValueError("input name and all the candidate names must be of type string.")
+    # Compute the fuzzy search score
+    fuzzy_scores = [fuzz.token_sort_ratio(
+        name, input_name) for name in candidate_names]
+
+    # Compute the cosine similarity score
+    vectorizer = CountVectorizer().fit_transform(
+        [input_name] + candidate_names)
+    cosine_similarities = cosine_similarity(
+        vectorizer[0:1], vectorizer[1:]).flatten()
+
+    # Combine the fuzzy search and cosine similarity scores
+    scores = [fuzzy_match * fuzzy_scores[i] + similarity *
+                cosine_similarities[i] for i in range(len(candidate_names))]
+
+    # Return the best matching name
+    return list(zip(candidate_names, scores))
 
 # %%
